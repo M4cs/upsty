4f2cf2bb6d0a72dbefc59b6068a5a65211e62cbc
M4cs <mabridgland@protonmail.com>
2019-11-07 19:17:01 -0500

    Add

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ Something went wrong!'''
 
 def upload_file(filename):
     try:
-        client.upload_file('tmp/{filename}'.format(filename=filename), app.config['S3_BUCKET_NAME'], '{}/{}'.format(app.config['S3_UPLOAD_DIR'], filename))
+        client.upload_file('tmp/{filename}'.format(filename=filename), app.config['S3_BUCKET_NAME'], '{}/{}'.format(app.config['S3_UPLOAD_DIR'], filename), ExtraArgs={'ACL':'public-read'})
         return True, 'Success'
     except Exception as e:
         return False, e
@@ -69,9 +69,5 @@ Error: {msg}'''.format(msg=msg)
     
 @app.route('/<uid>/<filename>', methods=['GET'])
 def send(uid, filename):
-    upload_dir = app.config['BASE_URL']
-    url = client.generate_presigned_url(ClientMethod="get_object",
-                                        Params={'Bucket': 'mbcdn',
-                                                'Key': '{upload_dir}/{uid}_{filename}'.format(upload_dir=app.config['BASE_URL'], uid=uid, filename=filename),
-                                                'ResponseContentDisposition': 'attachment; filename = {filename}'.format(filename=filename)})
+    url = 'https://mbcdn.sfo2.digitaloceanspaces.com/psty/{}_{}'.format(uid, filename)
     return redirect(url, 302)
